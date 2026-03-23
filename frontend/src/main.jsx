4d40ce756a04616422f22299576e4e1d557d8d6b
Elangoit001 <elangopoomi@gmail.com>
2026-03-23 23:07:05 +0530

    fix(tunnel): bypass loca.lt security warning screen instantly

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
+
+// Completely bypass LocalTunnel's annoying security warning page automatically!
+axios.defaults.headers.common['Bypass-Tunnel-Reminder'] = 'true';
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
